eaaae04a5e792f73716336cc14e08bba518a31f4
Enrico Bianco <enricob@gmail.com>
2013-10-06 16:04:02 -0700

    Canada is currently down (Parliament in Prorogue).
    See: http://o.canada.com/news/national/alert-governor-general-approves-harpers-request-to-prorogue-parliament/
    
    Canadian government to be operational again on Oct. 16 according to the
    article.

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
   }
 
   $.getJSON('http://api.wipmania.com/jsonp?callback=?', function (data) {
-    var down = ["United States", "Syrian Arab Republic"]
+    var down = ["Canada", "Syrian Arab Republic", "United States"]
     var country = data.address.country
     
     if (country in as_object(down)) {
